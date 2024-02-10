1f258d0fe703a5d01a5e75da8fc229bbff597510
saihnii4 <81787757+dulguuncodes@users.noreply.github.com>
2024-02-10 22:00:17 +0800

    init

---
+++ index.js
@@ -0,0 +1,36 @@
+const IMG_COUNT = 71;
+
+const loaded = Array(72).fill(false);
+
+const preload = (id) =>
+  new Promise((res, req) => {
+    if (loaded[id]) return req();
+    const im = new Image();
+    im.src = `/stop_motion/${id}.webp`;
+    im.onload = res;
+    im.onerror = req;
+  });
+
+window.onload = () => {
+  const target = document.querySelector("#focal");
+
+  const docE = document.documentElement;
+  [...Array(72).keys()].map((id) => {
+    preload(id)
+      .then(() => {
+        console.log();
+        console.log(`loaded ${id}`);
+        loaded[id] = true;
+      })
+      .catch(() => {});
+  });
+
+  window.onscroll = (ev) => {
+    const id =
+      Math.floor(IMG_COUNT*(docE.scrollTop / (docE.scrollHeight - docE.clientHeight)));
+
+    if (id < 0) return; // possible if overscrolling
+
+    target.src = `/stop_motion/${id}.webp`;
+  };
+};
